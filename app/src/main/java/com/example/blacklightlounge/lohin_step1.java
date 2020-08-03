f2c3b83c4733500e007bf2df422dc26c28f7ba7b
ceregik <ceregik10@yandex.ru>
2020-08-03 20:42:42 +0300

    Экран №2

--- app/src/main/java/com/example/blacklightlounge/lohin_step1.java
+++ app/src/main/java/com/example/blacklightlounge/lohin_step1.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class lohin_step1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lohin_step1);
-       // setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+
     }
+
 }
