83eb17d9e59c1b837014adaa1d5adbb80bc189ef
ceregik <ceregik10@yandex.ru>
2020-08-04 20:46:43 +0300

    all экраны

--- app/src/main/java/com/example/blacklightlounge/end_qr.java
+++ app/src/main/java/com/example/blacklightlounge/end_qr.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class end_qr extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_qr);
-       // setContentView(R.layout.activity_lohin_step1);
-       // setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
     }
 }
